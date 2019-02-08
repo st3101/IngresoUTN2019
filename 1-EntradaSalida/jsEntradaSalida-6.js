e1c4f498011ce8a96e0f3cc1045229b6fa65e9e8
Santiago Tomás Leonardi <47321301+st3101@users.noreply.github.com>
2019-02-08 12:29:35 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,17 @@ transformarlos a enteros (parseInt) y Sumarlos.
 Mostar el resulto por medio de "ALERT"*/
 function sumar()
 {
+    var numero1;
+    var numero2;
+    var resultado;
 
+numero1 = document.getElementById("numeroUno").value;
+numero1 = parseInt(numero1);
+
+numero2 = document.getElementById("numeroDos").value;
+numero2 = parseInt(numero2);
+
+resultado = (numero1 + numero2)
+
+alert("La suma es " + resultado);
 }
-
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -3,7 +3,20 @@ transformarlo a entero (parseInt), luego
 mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
-{
+{ 
+var sueldo;
+var aumento;
+var nuevoSueldo
 
+sueldo = document.getElementById("sueldo").value;
+sueldo = parseFloat(sueldo);
+
+aumento = (10 * sueldo / 100);
+
+document.getElementById("resultado").value = aumento;
+
+nuevoSueldo = aumento + sueldo
+
+alert("Su nuevo suledo total es " + nuevoSueldo);
 	
 }
