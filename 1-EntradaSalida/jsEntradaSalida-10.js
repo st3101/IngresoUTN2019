95613f2b004559dcda206e42c72f3ffdec3b1928
Santiago Tomás Leonardi <47321301+st3101@users.noreply.github.com>
2019-02-11 12:34:24 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,17 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarDescuento()
 {
+var importe;
+var aumento;
+var precioFinal;
+
+importe = parseFloat(document.getElementById("importe").value);
 	
+descuento = (importe + 25) / 100;
+
+precioFinal = importe - descuento;
+
+document.getElementById("resultado") = precioFinal;
+
+alert("Se aplico un descuento de $" + descuento);
 }
