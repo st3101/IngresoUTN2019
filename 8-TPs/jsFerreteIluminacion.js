d7265bc4a3477a68a26411c898450e45f2033569
Santiago Tomás Leonardi <47321301+st3101@users.noreply.github.com>
2019-02-18 00:15:57 -0300

    Add files via upload

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -5,10 +5,59 @@ B.	Si compra 5  lamparitas bajo consumo marca "ArgentinaLuz" se hace un descuent
 C.	Si compra 4  lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas” se hace un descuento del 25 % y si es de otra marca el descuento es del 20%.
 D.	Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz"  el descuento es del 15%, si es  “FelipeLamparas” se hace un descuento del 10 % y si es de otra marca un 5%.
 E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de ingresos brutos en informar del impuesto con el siguiente mensaje:
- ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
+ ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. */
 
- */
-function CalcularPrecio () 
-{
- 	
+function CalcularPrecio() {
+
+    var cantidad
+    var precio
+    var precioTotal;
+    var descuento
+    var marca;
+    var iibb
+    var iibbFinal
+
+    cantidad = parseInt(document.getElementById("Cantidad").value);
+    marca = document.getElementById("Marca").value;
+
+    precio = cantidad * 35;
+
+    if (cantidad >= 6) {
+
+        descuento = precio / 2;
+    }
+    else if (cantidad == 5 && marca == "ArgentinaLuz") {
+
+        descuento = precio * 0.4;
+    }
+    else if (cantidad == 5 && marca != "ArgentinaLuz") {
+
+        descuento = precio * 0.3;
+    }
+    else if (cantidad == 4 && (marca == "ArgentinaLuz" || marca == "FelipeLamparas")) {
+
+        descuento = precio * 0.25;
+    }
+    else if (cantidad == 4 != (marca == "ArgentinaLuz" || marca == "FelipeLamparas")) {
+
+        descuento = precio * 0.2;
+    }
+    else if (cantidad == 3 && marca == "ArgentinaLuz") {
+        descuento = precio * 0.15;
+    }
+    else if (cantidad == 3 && marca == "FelipeLamparas") {
+        descuento = precio * 0.1;
+    }
+    else if (cantidad == 3 != (marca == "ArgentinaLuz" && marca == "FelipeLamparas")) {
+        descuento = precio * 0.05;
+    }
+    
+    precioTotal = precio - descuento
+    if(precioTotal >= 120){
+        iibb = precioTotal * 0.1;
+        iibbFinal = precioTotal - iibb
+        alert("IIBB Usted pago $" + precioTotal + " siendo $" + iibbFinal + " el impuesto que se pagó");
+    }
+    
+    document.getElementById("precioDescuento").value = precioTotal.toFixed(2);
 }
